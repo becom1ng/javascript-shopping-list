f99ccd3cf5b648dc17494d75af42b5f2ba6a8454
becom1ng <becom1ng@users.noreply.github.com>
2024-01-24 11:28:45 -0600

    added edit mode

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearButton = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
+const formButton = itemForm.querySelector('button');
+let isEditMode = false;
 
 function displayItems() {
 	const itemsFromStorage = getItemsFromStorage();
@@ -81,7 +83,22 @@ function getItemsFromStorage() {
 function onClickItem(e) {
 	if (e.target.parentElement.classList.contains('remove-item')) {
 		removeItem(e.target.parentElement.parentElement);
+	} else {
+		setItemToEdit(e.target);
 	}
+}
+
+function setItemToEdit(item) {
+	isEditMode = true;
+
+	itemList
+		.querySelectorAll('li')
+		.forEach((i) => i.classList.remove('edit-mode'));
+
+	item.classList.add('edit-mode');
+	formButton.innerHTML = '<i class ="fa-solid fa-pen"></i> Update Item';
+	formButton.style.backgroundColor = 'green';
+	itemInput.value = item.textContent;
 }
 
 function removeItem(item) {
